8073afc047532aaaaa3e845d1acd31b720a4f3a8
alejandro250517 <brayanmcruz.425@gmail.com>
2025-10-07 21:00:53 -0500

    Se configuran archivos que tienen requerido el bcryptjs

--- src/database.js
+++ src/database.js
@@ -1,6 +1,6 @@
 require('dotenv').config();
 const mongoose = require('mongoose');
-const bcrypt = require('bcrypt');
+const bcrypt = require('bcryptjs');
 const Usuario = require('../src/models/Usuario');
 // Conexión a MongoDB
 mongoose.connect(process.env.MONGO_URI)
